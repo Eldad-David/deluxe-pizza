7d0666699d6e8a27529020771276565b0337cde6
Eldad David <davengash100@gmail.com>
2021-06-23 15:15:10 +0300

    add box divs to menu

--- js/index.js
+++ js/index.js
@@ -4,3 +4,47 @@ $(document).ready(function() {
         $(".pizza1-text").slideDown('slow');
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//...
+@media screen and (max-width: 992px) {
+    .pizza1 {
+        width: 200px;
+        height: 200px;
+    }
+    .pizza2 {
+        width: 200px;
+        height: 200px;
+    }
+    .pizza3 {
+        width: 200px;
+        height: 200px;
+    }   
+}
+@media screen and (max-width: 760px) {
+    .pizza1 {
+        width: 180px;
+        height: 180px;
+    }
+    .pizza2 {
+        width: 180px;
+        height: 180px;
+    }
+    .pizza3 {
+        width: 180px;
+        height: 180px;
+    }
+}
+..//
