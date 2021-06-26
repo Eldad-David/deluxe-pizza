f2de039f0c1c9883f6eb15c79e359a3244da5260
Eldad David <davengash100@gmail.com>
2021-06-26 19:43:18 +0300

    add j query to the menu section

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
-// user interface code
+// user interface logic
 $(document).ready(function() {
-    $('.quantity').if (condition) {
-        
-    }
+    $('.pizza1').click(function() {
+        $('.pizza1-text').slideDown('slow');
+    });
+    $('.pizza2').click(function() {
+        $('.pizza2-text').slideDown('slow');
+    });
+    $('.pizza3').click(function() {
+        $('.pizza3-text').slideDown('slow');
+    });
+
+
+    $('.btn-cart').click(function() {
+        alert('Your order has been put in the cart successfully!');
+    });
+    
 });
-
-
-function () {
-
-}
